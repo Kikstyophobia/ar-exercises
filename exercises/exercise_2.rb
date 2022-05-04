d1ea571c0c390cb1b1d5a51d301e70eea35fa810
Calvin Chung <calvin-chung@live.com>
2022-05-03 20:02:01 -0700

    exercises 1-4 complete with queries

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,9 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(id: "1")
+@store1.name = "new store name"
+@store1.save
+puts @store1.name
+
+@store2 = Store.find_by(id: "2")
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,20 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true, mens_apparel: false)
+Store.create(name: "Whistler", annual_revenue: 1900000, womens_apparel: false, mens_apparel: true)
+Store.create(name: "Yaletown", annual_revenue: 430000, womens_apparel: true, mens_apparel: true)
+puts "Number of stores: #{Store.count}" 
+@mens_stores = Store.where(womens_apparel: false, mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "#{store.name}"
+  puts "#{store.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: true, annual_revenue: (...1000000000))
+
+@womens_stores.each do |store|
+  puts "#{store.name}"
+  puts "#{store.annual_revenue}"
+end
